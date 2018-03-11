02b196cd3d05719db25c0a6dd57e938184e51944
SvetlanaTomic <svetlana.tomic997@gmail.com>
2018-03-11 02:28:23 +0100

    dodata klasa koja implentira IMenjacnica

---
+++ Menjacnica/src/menjacnica/ImpelemtacijaIMenjacnica.java
@@ -0,0 +1,28 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import menjacnica.valuta.KursZaDatum;
+import menjacnicaInrefejs.IMenjacnica;
+
+public class ImpelemtacijaIMenjacnica implements IMenjacnica{
+
+	@Override
+	public void dodajKursZaDatum(KursZaDatum k) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void obrisiKursZaDatum(GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public KursZaDatum vratiKursZaDatum(GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
