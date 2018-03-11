603e04fcd919f7709fc2636dbace1acfbec4eeab
SvetlanaTomic <svetlana.tomic997@gmail.com>
2018-03-11 01:20:33 +0100

    Dodane metode toStirng, equals and hashCode u klase Menjacnica, Valuta i KursZaDatum

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -15,5 +15,38 @@ public class Menjacnica {
 	public void setValute(LinkedList<Valuta> valute) {
 		this.valute = valute;
 	}
+
+	@Override
+	public String toString() {
+		String poruka="";
+		for(int i=0;i<valute.size();i++)
+			poruka+=valute.get(i).getNaziv();
+		return poruka;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((valute == null) ? 0 : valute.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Menjacnica))
+			return false;
+		Menjacnica other = (Menjacnica) obj;
+		if (valute == null) {
+			if (other.valute != null)
+				return false;
+		} else if (!valute.equals(other.valute))
+			return false;
+		return true;
+	}
 	
 }
--- Menjacnica/src/menjacnica/valuta/Valuta.java
+++ Menjacnica/src/menjacnica/valuta/Valuta.java
@@ -25,4 +25,47 @@ public class Valuta {
 	public void setKursZaDatum(LinkedList<KursZaDatum> kursZaDatum) {
 		this.kursZaDatum = kursZaDatum;
 	}
+	@Override
+	public String toString() {
+		return "Valuta [naziv=" + naziv + ", skraceniNaziv=" + skraceniNaziv
+				+ ", kursZaDatum=" + kursZaDatum + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((kursZaDatum == null) ? 0 : kursZaDatum.hashCode());
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		result = prime * result
+				+ ((skraceniNaziv == null) ? 0 : skraceniNaziv.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Valuta))
+			return false;
+		Valuta other = (Valuta) obj;
+		if (kursZaDatum == null) {
+			if (other.kursZaDatum != null)
+				return false;
+		} else if (!kursZaDatum.equals(other.kursZaDatum))
+			return false;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		if (skraceniNaziv == null) {
+			if (other.skraceniNaziv != null)
+				return false;
+		} else if (!skraceniNaziv.equals(other.skraceniNaziv))
+			return false;
+		return true;
+	}
+	
 }
